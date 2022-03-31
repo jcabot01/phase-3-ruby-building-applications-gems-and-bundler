ef31e1c950c3f4d3ca7434cca12f7828ab015bd1
JonCabot <jfc0053@gmail.com>
2022-03-30 20:51:57 -0500

    commit tree d135c0835eea47d9a04b436c9755ced18afa0f2b

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,13 @@
 # Set up Bundler here following the instructions in the README
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
+
+# # this code
+# require 'bundler/setup'
+# Bundler.require(:default, :development)
+
+# # is equivalent to this code
+# require 'rspec'
+# require 'rest-client'
+# require 'pry'
